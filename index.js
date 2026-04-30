79408dd5cfcf15cb2bdcdbfc49c0dd1d8a4d8819
Enrico <e.giacomazzi@contralto-audio.com>
2026-04-30 11:31:51 +0200

    auth implemented

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import Fastify from "fastify";
 import fastifyMysql from "@fastify/mysql";
 import fastifyStatic from "@fastify/static";
 import fastifyView from "@fastify/view";
+import fastifyFormbody from "@fastify/formbody";
+import fastifyCookie from "@fastify/cookie";
+import fastifySession from "@fastify/session";
 import { join } from "desm";
 import ejs from 'ejs';
 
@@ -43,18 +46,48 @@ await app.register(fastifyMysql, {
     promise: true
 });
 
+await app.register(fastifyFormbody);
+
+await app.register(fastifyCookie, {
+    secret: 'dfsdfgsdfg'
+});
+
+await app.register(fastifySession, {
+    secret: 'hgtmr572guw94m10d75nqwezapf86v35nto2',
+    cookie: {
+        secure: false,
+        // httpOnly: true,
+        // sameSite: 'lax',
+        // path: '/',
+        // maxAge: 1000 * 60 * 60
+    },
+    saveUninitialized: false
+});
+
 // ---------------------------------------------------------------------------------------
 
 // registro le rotte ---------------------------------------------------------------------
 
-// pagina rpincipale
+// pagina principale
 app.get('/', async (req, res) => {
+
+    // validazione utente loggato
+    if(!req.session.user) {
+        return res.redirect('/login.html');
+    }
+
     // recuparo la connessione al DB
     const connection = await app.mysql.getConnection();
+
     // eseguo la query, il risultato sarà un array, il cui primo elemento e a sua volta
     // un array di oggetti che rappresentano le righe della query
     // le proprietà dei singoli oggetti sono il nome delle colonne
-    const results = await connection.query('SELECT * FROM commesse;'); 
+    const results = await connection.query(
+        `select c.id_commessa, c.codice_prodotto, c.quantita_prevista, c.stato_commessa, 
+        sum(p.pezzi_prodotti) - sum(p.pezzi_scartati) as totale_prodotti
+        from commesse c left join produzione p on c.id_commessa = p.id_commessa
+        group by c.id_commessa`
+    ); 
     // renderizzo la pagina passando le commesse
     return res.view('commesse.ejs', {commesse: results[0]});
 });
@@ -63,6 +96,12 @@ app.get('/', async (req, res) => {
 // pagina dettaglio: specificando nel path un elemento con i due punti, posso usare
 // quell'elemento come parametro, in questo caso il parametro si chiama id
 app.get('/commesse/:id', async (req, res) => {
+
+    if(!req.session.user) {
+        return res.redirect('/login.html');
+    }
+
+
     // recupero l'id commessa
     const id = req.params.id;
     // recuparo la connessione al DB
@@ -75,9 +114,32 @@ app.get('/commesse/:id', async (req, res) => {
     const codice = tmp[0][0].codice_prodotto;
 
     // recupero le righe di produzione
-    const results = await connection.query('SELECT * FROM produzione WHERE id_commessa = ?;', [id]); 
+    const results = await connection.query('SELECT * FROM produzione WHERE id_commessa = ? AND stato_macchina = \'Running\';', [id]); 
     return res.view('dettaglio.ejs', {codice: codice, produzione: results[0]});
 });
+
+
+app.post('/pippo', async (req, res) => {
+
+    const username = req.body.username;
+    const password = req.body.password;
+    const connection = await app.mysql.getConnection();
+    const results = await connection.query(
+        `SELECT * FROM utenti WHERE username = ? AND password = ?;`, [username, password]
+    );
+
+    const user = results[0][0];
+
+    if(!user) {
+        res.redirect('/unauth.html');
+        return;
+    }
+
+
+    req.session.user = user;
+    res.redirect('/');
+});
+
 
 
 // ---------------------------------------------------------------------------------------
